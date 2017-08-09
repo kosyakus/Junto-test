f5f235031d28f5fe146690f3de362dedf28f7a71
Admin <kosyak_@mail.ru>
2017-08-09 13:39:31 +0300

    Initial Commit

---
+++ Junto test task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Junto test task
+//
+//  Created by Admin on 09.08.17.
+//  Copyright © 2017 NS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
